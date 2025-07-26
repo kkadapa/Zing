3fec8aa659402280b99d13a9937c51afd33c033c
Karthik <karthikkadapa88@gmail.com>
2025-07-26 12:08:46 -0500

    Initial Commit

---
+++ Zing/ZingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZingApp.swift
+//  Zing
+//
+//  Created by Nathan Drake on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ZingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
